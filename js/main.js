0689226f70a6f9bd23b9f8c1fd42899e68f01e51
Надежда <77618567+DavlatovaN@users.noreply.github.com>
2021-06-18 22:26:39 +0800

    внесла правки

--- js/main.js
+++ js/main.js
@@ -13,12 +13,6 @@ const CHECK_IN_TIME = [
   '14:00',
 ];
 
-const CHECK_OUT_TIME = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
 const ALL_FEATURES = [
   'wifi',
   'dishwasher',
@@ -26,6 +20,15 @@ const ALL_FEATURES = [
   'washer',
   'elevator',
 ];
+const MIN_PRICE = 10000;
+const MAX_PRICE = 100000;
+
+const MIN_ROOM = 1;
+const MAX_ROOM = 100;
+
+const MIN_GUEST = 1;
+const MAX_GUEST = 100;
+
 
 const ALL_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
@@ -34,40 +37,43 @@ const ALL_PHOTOS = [
 ];
 
 const SIMILAR_COUNT = 10;
-const LATITUDE = {
-  min: 35.65000,
-  max: 35.70000,
-};
-const LONGITUDE = {
-  min: 139.70000,
-  max: 139.80000,
-};
 
-const createAd = () => ({
-  author: {
-    avatar: `img/avatars/user0${getRandomPositiveInteger(1, 8)}.png`,
-  },
-  location: {
-    lat: randomLocation.lat,
-    lng: randomLocation.lng,
-  },
-  offer: {
-    title: 'Заголовок объявления',
-    address: `${location.lat  }, ${  location.lng}`, //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
-    price: getRandomPositiveInteger(10000, 1000000),
-    type: getRandomArrayElement(TYPES_OF_HOUSING),
-    rooms: getRandomPositiveInteger(1, 100),
-    guests: getRandomPositiveInteger(1, 100),
-    checkin: getRandomArrayElement(CHECK_IN_TIME),
-    checkout: getRandomArrayElement(CHECK_OUT_TIME),
-    features: createArr(ALL_FEATURES, 5),
-    description: 'Описание объявления',
-    photos: createArr(ALL_PHOTOS, 3),
-  },
+// функция получения случайных координат для объекта 'location' и 'offer.address"
+const getRandomLocation = function () {
+  return {
+    lat: getRandomValueFloat(35.65000, 35.70000, 5),
+    lng: getRandomValueFloat(139.70000, 139.80000, 5),
+  };
+};
+const createAd= function () {
+  const randomLocation = getRandomLocation();
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandomPositiveInteger(1, 8)}.png`,
+    },
+
+    location: {
+      lat: randomLocation.lat,
+      lng: randomLocation.lng,
+    },
+
+    offer: {
+      title: 'Заголовок объявления',
+      address: `${randomLocation.lat}, ${randomLocation.lng}`, //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
+      price: getRandomPositiveInteger(MIN_PRICE, MAX_PRICE),//число — стоимость. Случайное целое положительное число.
+      type: getRandomArrayElement(TYPES_OF_HOUSING),
+      rooms: getRandomPositiveInteger(MIN_ROOM, MAX_ROOM),
+      guests: getRandomPositiveInteger(MIN_GUEST, MAX_GUEST),
+      checkin: getRandomArrayElement(CHECK_IN_TIME),
+      checkout: getRandomArrayElement(CHECK_IN_TIME),
+      features: createArr(ALL_FEATURES, 5),
+      description: 'Описание объявления',
+      photos: createArr(ALL_PHOTOS, 3),
+    },
 });
 
 
-const similarAd  = new Array(SIMILAR_COUNT).fill(null).map(createAd);
-
-similarAd;
-
+const similarAd = function (similar_count) {
+  return new Array(similar_count).fill(null).map(createAd);
+}
+similarAd
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
-function getRandomPositiveFloat (min, max, digits = 1) {
+function getRandomValueFloat (min, max, digits = 1) {
   const lower = Math.min(Math.abs(min), Math.abs(max));
   const upper = Math.max(Math.abs(min), Math.abs(max));
   const result = Math.random() * (upper - lower) + lower;
   return result.toFixed(digits);
 }
 
-getRandomPositiveFloat;
+getRandomValueFloat;
 
 function getRandomPositiveInteger (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -19,7 +19,7 @@ const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0,
 
 getRandomArrayElement;
 
-// массив случайной длины из неповторяющихся значений. Взял с https://qna.habr.com/q/844269
+// массив случайной длины из неповторяющихся значений. Взяла с https://qna.habr.com/q/844269
 const createArr = ([...source], maxLength) => Array.from(
   { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
   () => source.splice(Math.random() * source.length | 0, 1)[0]);
