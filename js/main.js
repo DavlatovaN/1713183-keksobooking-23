ee463ec204145cf4f83fe657c800154a2c9a25ce
Надежда <77618567+DavlatovaN@users.noreply.github.com>
2021-06-25 16:46:02 +0800

    Модуляция-3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,5 @@ import {createAd, similarAd}
   from './data.js';
 
 const SIMILAR_COUNT = 10;
-const similarAd = function (SIMILAR_COUNT) {
-  return new Array(SIMILAR_COUNT).fill(null).map(createAd);
-}
+
 console.log(similarAd(SIMILAR_COUNT));
